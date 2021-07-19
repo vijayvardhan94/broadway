f73c6796c8096cf77aba2db20969bf7bb99e4bee
vijayvardhan94 <tadimetv@oregonstate.edu>
2021-07-18 23:15:59 -0700

    updated root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/pages/home' => 'pages#home' 
+  root 'pages#home'
+  #get '/pages/home' => 'pages#home' 
 end
